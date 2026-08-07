2f3a3558ef08b990e8fc15d74f708076590f8bda
bc1cindy <bc1cindy@proton.me>
2026-08-07 13:22:35 -0300

    export the four chain-analysis channels from the package

--- decluster/__init__.py
+++ decluster/__init__.py
@@ -4,3 +4,7 @@ from .extractors import x_nsequence, x_input_order, x_io_shape
 from .engine import measure, print_report, sample_recent_txs, locktime_class
 from .combiner import Combiner
 from .cluster import cluster_naive, cluster_refined
+from .conservation import forced_in_round
+from .provenance import candidate_coins, rank_by_overlap
+from .monitor import walk_frontier
+from .intersect import evaluate, score_candidate
